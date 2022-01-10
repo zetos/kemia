f03ce4e432c305302b8108f336bd60f10731e083
Zetos Lima <cesar_manoel@hotmail.com>
2022-01-10 19:27:31 -0300

    test: add a diluted recipe.

--- src/index.test.ts
+++ src/index.test.ts
@@ -42,6 +42,25 @@ const rcp3: Recipe = {
   ],
 };
 
+const rcp4: Recipe = {
+  base: {
+    name: 'Sea Dew Leaves',
+    qtd: 11,
+    Dh: 1.2,
+    Aw: 1,
+    DhMult: 0,
+  },
+  multipliers: [
+    {
+      name: 'Muse Fruit',
+      qtd: 100,
+      Dh: 0.15,
+      DhMult: 0.44,
+      Aw: 1,
+    },
+  ],
+};
+
 test('PDH of 11 Sea Dew Leaves returns 2.4', (t) => {
   t.is(mix(rcp1).pdh, 2.4);
 });
@@ -52,4 +71,8 @@ test('PDH of 11 Muse Fruit returns 0.432', (t) => {
 
 test('PDH of 11 Sea Dew Leaves + 1 Muse Fruit returns 2.508', (t) => {
   t.is(mix(rcp3).pdh, 2.508);
+});
+
+test('PDH of 11 Sea Dew Leaves + 100 Muse Fruit returns 0.72', (t) => {
+  t.is(mix(rcp4).pdh, 0.72);
 });
