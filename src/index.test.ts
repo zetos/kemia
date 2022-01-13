732dbc92b4f24ff799788fef87fbc9abe5ec514c
Zetos Lima <cesar_manoel@hotmail.com>
2022-01-13 03:06:11 -0300

    test: add pu example.

--- src/index.test.ts
+++ src/index.test.ts
@@ -61,6 +61,25 @@ const rcp4: Recipe = {
   ],
 };
 
+const rcp5: Recipe = {
+  base: {
+    name: 'Salvia',
+    qtd: 85,
+    dh: 1.8,
+    aw: 1,
+    dhm: 0,
+  },
+  multipliers: [
+    {
+      name: 'Muse Fruit',
+      qtd: 16,
+      dh: 0.15,
+      dhm: 0.44,
+      aw: 1,
+    },
+  ],
+};
+
 test('PDH of 11 Sea Dew Leaves returns 2.4', (t) => {
   t.is(mix(rcp1).pdh, 2.4);
 });
@@ -75,4 +94,8 @@ test('PDH of 11 Sea Dew Leaves + 1 Muse Fruit returns 2.508', (t) => {
 
 test('PDH of 11 Sea Dew Leaves + 100 Muse Fruit returns 0.72', (t) => {
   t.is(mix(rcp4).pdh, 0.72);
+});
+
+test('PU of 85 Salvia + 16 Muse Fruit returns 10', (t) => {
+  t.is(mix(rcp5).pu, 10);
 });
