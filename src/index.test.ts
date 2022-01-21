9a8d672dbbf7913ebf15177e62fe837f10c8b64c
Zetos Lima <cesar_manoel@hotmail.com>
2022-01-21 00:06:38 -0300

    refactor: remove base & multiplier objects.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,337 +1,317 @@
 import test from 'ava';
-import { mix, calcPotionUnit, Recipe } from './index';
+import { mix, calcPotionUnit, Ingredient } from './index';
 
-const rcp1: Recipe = {
-  base: {
-    name: 'Sea Dew Leaves',
-    qtd: 11,
-    pdh: 1.2,
-    aw: 1,
-    dhm: 0,
-  },
-  multipliers: [],
-};
+const rcp1: Ingredient[] = [
+  { name: 'Sea Dew Leaves', qtd: 11, pdh: 1.2, aw: 1, dhm: 0 },
+];
 
-const rcp2: Recipe = {
-  base: {
+const rcp2: Ingredient[] = [
+  {
     name: 'Muse Fruit',
     qtd: 1,
     pdh: 0.15,
     dhm: 0.44,
     aw: 1,
   },
-  multipliers: [],
-};
+];
 
-const rcp3: Recipe = {
-  base: {
+const rcp3: Ingredient[] = [
+  {
     name: 'Sea Dew Leaves',
     qtd: 11,
     pdh: 1.2,
     aw: 1,
     dhm: 0,
   },
-  multipliers: [
-    {
-      name: 'Muse Fruit',
-      qtd: 1,
-      pdh: 0.15,
-      dhm: 0.44,
-      aw: 1,
-    },
-  ],
-};
+  {
+    name: 'Muse Fruit',
+    qtd: 1,
+    pdh: 0.15,
+    dhm: 0.44,
+    aw: 1,
+  },
+];
 
-const rcp4: Recipe = {
-  base: {
+const rcp4: Ingredient[] = [
+  {
     name: 'Sea Dew Leaves',
     qtd: 11,
     pdh: 1.2,
     aw: 1,
     dhm: 0,
   },
-  multipliers: [
-    {
-      name: 'Muse Fruit',
-      qtd: 100,
-      pdh: 0.15,
-      dhm: 0.44,
-      aw: 1,
-    },
-  ],
-};
+  {
+    name: 'Muse Fruit',
+    qtd: 100,
+    pdh: 0.15,
+    dhm: 0.44,
+    aw: 1,
+  },
+];
 
-const rcp5: Recipe = {
-  base: {
+const rcp5: Ingredient[] = [
+  {
     name: 'Salvia',
     qtd: 85,
     pdh: 1.8,
     aw: 1,
     dhm: 0,
   },
-  multipliers: [
-    {
-      name: 'Muse Fruit',
-      qtd: 16,
-      pdh: 0.15,
-      dhm: 0.44,
-      aw: 1,
-    },
-  ],
-};
+  {
+    name: 'Muse Fruit',
+    qtd: 16,
+    pdh: 0.15,
+    dhm: 0.44,
+    aw: 1,
+  },
+];
 
-const rcp6: Recipe = {
-  base: {
+const rcp6: Ingredient[] = [
+  {
     name: 'Salvia Oil',
     qtd: 285,
     pdh: 2.1,
     aw: 1,
     dhm: 0,
   },
-  multipliers: [
-    {
-      name: 'Argus Sponge',
-      qtd: 26,
-      pdh: 0,
-      aw: 1,
-      dhm: 0.96,
-    },
-    {
-      name: 'Skadite',
-      qtd: 26,
-      pdh: 0,
-      aw: 0,
-      dhm: 0.96,
-    },
-    {
-      name: 'Jadeite',
-      qtd: 16,
-      pdh: 0,
-      aw: 0,
-      dhm: 0.69,
-    },
-    {
-      name: 'Nitre Queen Carcass',
-      qtd: 16,
-      pdh: 0,
-      aw: 1,
-      dhm: 0.69,
-    },
-    {
-      name: 'Great Horn',
-      qtd: 16,
-      pdh: 0,
-      aw: 0,
-      dhm: 0.69,
-    },
-    {
-      name: 'Gold',
-      qtd: 16,
-      pdh: 0,
-      aw: 0,
-      dhm: 0.69,
-    },
-    {
-      name: 'Pirum Juice',
-      qtd: 11,
-      pdh: 0,
-      aw: 1,
-      dhm: 0.56,
-    },
-    {
-      name: 'Electrum',
-      qtd: 11,
-      pdh: 0,
-      aw: 0,
-      dhm: 0.56,
-    },
-    {
-      name: 'Green Jambura Juice',
-      qtd: 11,
-      pdh: 0,
-      aw: 1,
-      dhm: 0.56,
-    },
-    {
-      name: 'Muse Fruit',
-      qtd: 8,
-      pdh: 0.15,
-      aw: 1,
-      dhm: 0.44,
-    },
-    {
-      name: 'Green Jamburra',
-      qtd: 7,
-      pdh: 0,
-      aw: 1,
-      dhm: 0.44,
-    },
-    {
-      name: 'Pirum',
-      qtd: 7,
-      pdh: 0,
-      aw: 1,
-      dhm: 0.44,
-    },
-    {
-      name: 'Blood Ore',
-      qtd: 7,
-      pdh: 0,
-      aw: 0,
-      dhm: 0.44,
-    },
-    {
-      name: 'Compact Horn',
-      qtd: 7,
-      pdh: 0,
-      aw: 0,
-      dhm: 0.44,
-    },
-    {
-      name: 'Basileus',
-      qtd: 7,
-      pdh: 0,
-      aw: 1,
-      dhm: 0.44,
-    },
-  ],
-};
+  {
+    name: 'Argus Sponge',
+    qtd: 26,
+    pdh: 0,
+    aw: 1,
+    dhm: 0.96,
+  },
+  {
+    name: 'Skadite',
+    qtd: 26,
+    pdh: 0,
+    aw: 0,
+    dhm: 0.96,
+  },
+  {
+    name: 'Jadeite',
+    qtd: 16,
+    pdh: 0,
+    aw: 0,
+    dhm: 0.69,
+  },
+  {
+    name: 'Nitre Queen Carcass',
+    qtd: 16,
+    pdh: 0,
+    aw: 1,
+    dhm: 0.69,
+  },
+  {
+    name: 'Great Horn',
+    qtd: 16,
+    pdh: 0,
+    aw: 0,
+    dhm: 0.69,
+  },
+  {
+    name: 'Gold',
+    qtd: 16,
+    pdh: 0,
+    aw: 0,
+    dhm: 0.69,
+  },
+  {
+    name: 'Pirum Juice',
+    qtd: 11,
+    pdh: 0,
+    aw: 1,
+    dhm: 0.56,
+  },
+  {
+    name: 'Electrum',
+    qtd: 11,
+    pdh: 0,
+    aw: 0,
+    dhm: 0.56,
+  },
+  {
+    name: 'Green Jambura Juice',
+    qtd: 11,
+    pdh: 0,
+    aw: 1,
+    dhm: 0.56,
+  },
+  {
+    name: 'Muse Fruit',
+    qtd: 8,
+    pdh: 0.15,
+    aw: 1,
+    dhm: 0.44,
+  },
+  {
+    name: 'Green Jamburra',
+    qtd: 7,
+    pdh: 0,
+    aw: 1,
+    dhm: 0.44,
+  },
+  {
+    name: 'Pirum',
+    qtd: 7,
+    pdh: 0,
+    aw: 1,
+    dhm: 0.44,
+  },
+  {
+    name: 'Blood Ore',
+    qtd: 7,
+    pdh: 0,
+    aw: 0,
+    dhm: 0.44,
+  },
+  {
+    name: 'Compact Horn',
+    qtd: 7,
+    pdh: 0,
+    aw: 0,
+    dhm: 0.44,
+  },
+  {
+    name: 'Basileus',
+    qtd: 7,
+    pdh: 0,
+    aw: 1,
+    dhm: 0.44,
+  },
+];
 
-const rcp7: Recipe = {
-  base: {
+const rcp7: Ingredient[] = [
+  {
     name: 'Sea Dew Leaves',
     qtd: 321,
     pdh: 1.2,
     aw: 1,
     dhm: 0,
   },
-  multipliers: [
-    {
-      name: 'Gold',
-      qtd: 20,
-      pdh: 0,
-      dhm: 0.69,
-      aw: 0,
-    },
-    {
-      name: 'Pirum Juice',
-      qtd: 13,
-      pdh: 0,
-      dhm: 0.56,
-      aw: 1,
-    },
-    {
-      name: 'Electrum',
-      qtd: 13,
-      pdh: 0,
-      dhm: 0.56,
-      aw: 0,
-    },
-    {
-      name: 'Green Jamburra Juice',
-      qtd: 13,
-      pdh: 0,
-      dhm: 0.56,
-      aw: 1,
-    },
-    {
-      name: 'Gamum Oil',
-      qtd: 10,
-      pdh: 0,
-      dhm: 0.44,
-      aw: 1,
-    },
-    {
-      name: 'Compact Horn',
-      qtd: 10,
-      pdh: 0,
-      dhm: 0.44,
-      aw: 0,
-    },
-    {
-      name: 'Basileus',
-      qtd: 10,
-      pdh: 0,
-      dhm: 0.44,
-      aw: 1,
-    },
-    {
-      name: 'Kyanite',
-      qtd: 10,
-      pdh: 0,
-      dhm: 0.44,
-      aw: 0,
-    },
-    {
-      name: 'Pirum',
-      qtd: 10,
-      pdh: 0,
-      dhm: 0.44,
-      aw: 1,
-    },
-    {
-      name: 'Waterstone',
-      qtd: 10,
-      pdh: 0,
-      dhm: 0.44,
-      aw: 0,
-    },
-    {
-      name: 'White Bear Carcass',
-      qtd: 10,
-      pdh: 0,
-      dhm: 0.44,
-      aw: 1,
-    },
-    {
-      name: 'Blood Ore',
-      qtd: 10,
-      pdh: 0,
-      dhm: 0.44,
-      aw: 0,
-    },
-    {
-      name: 'Green Jamburra',
-      qtd: 10,
-      pdh: 0,
-      dhm: 0.44,
-      aw: 1,
-    },
-    {
-      name: 'Muse Fruit',
-      qtd: 10,
-      pdh: 0.15,
-      dhm: 0.44,
-      aw: 1,
-    },
-  ],
-};
+  {
+    name: 'Gold',
+    qtd: 20,
+    pdh: 0,
+    dhm: 0.69,
+    aw: 0,
+  },
+  {
+    name: 'Pirum Juice',
+    qtd: 13,
+    pdh: 0,
+    dhm: 0.56,
+    aw: 1,
+  },
+  {
+    name: 'Electrum',
+    qtd: 13,
+    pdh: 0,
+    dhm: 0.56,
+    aw: 0,
+  },
+  {
+    name: 'Green Jamburra Juice',
+    qtd: 13,
+    pdh: 0,
+    dhm: 0.56,
+    aw: 1,
+  },
+  {
+    name: 'Gamum Oil',
+    qtd: 10,
+    pdh: 0,
+    dhm: 0.44,
+    aw: 1,
+  },
+  {
+    name: 'Compact Horn',
+    qtd: 10,
+    pdh: 0,
+    dhm: 0.44,
+    aw: 0,
+  },
+  {
+    name: 'Basileus',
+    qtd: 10,
+    pdh: 0,
+    dhm: 0.44,
+    aw: 1,
+  },
+  {
+    name: 'Kyanite',
+    qtd: 10,
+    pdh: 0,
+    dhm: 0.44,
+    aw: 0,
+  },
+  {
+    name: 'Pirum',
+    qtd: 10,
+    pdh: 0,
+    dhm: 0.44,
+    aw: 1,
+  },
+  {
+    name: 'Waterstone',
+    qtd: 10,
+    pdh: 0,
+    dhm: 0.44,
+    aw: 0,
+  },
+  {
+    name: 'White Bear Carcass',
+    qtd: 10,
+    pdh: 0,
+    dhm: 0.44,
+    aw: 1,
+  },
+  {
+    name: 'Blood Ore',
+    qtd: 10,
+    pdh: 0,
+    dhm: 0.44,
+    aw: 0,
+  },
+  {
+    name: 'Green Jamburra',
+    qtd: 10,
+    pdh: 0,
+    dhm: 0.44,
+    aw: 1,
+  },
+  {
+    name: 'Muse Fruit',
+    qtd: 10,
+    pdh: 0.15,
+    dhm: 0.44,
+    aw: 1,
+  },
+];
 
-const rcp8: Recipe = {
-  base: {
+const rcp8: Ingredient[] = [
+  {
     name: 'Sea Dew Leaves',
     qtd: 404,
     pdh: 1.2,
     aw: 1,
     dhm: 0,
   },
-  multipliers: [
-    {
-      name: 'Gold',
-      qtd: 16,
-      pdh: 0,
-      dhm: 0.69,
-      aw: 0,
-    },
-    {
-      name: 'Pirum Juice',
-      qtd: 8,
-      pdh: 0,
-      dhm: 0.56,
-      aw: 1,
-    },
-  ],
-};
+  {
+    name: 'Gold',
+    qtd: 16,
+    pdh: 0,
+    dhm: 0.69,
+    aw: 0,
+  },
+  {
+    name: 'Pirum Juice',
+    qtd: 8,
+    pdh: 0,
+    dhm: 0.56,
+    aw: 1,
+  },
+];
 
 test('PDH of 11 Sea Dew Leaves returns 2.4', (t) => {
   t.is(mix(rcp1).pdh, 2.4);
@@ -357,80 +337,75 @@ test('PU of 85 Salvia + 16 Muse Fruit returns 10', (t) => {
 
 test('PU of 10 Purified Water returns 0', (t) => {
   t.is(
-    calcPotionUnit({
-      base: {
+    calcPotionUnit([
+      {
         name: 'Purified Water',
         qtd: 10,
         pdh: 0,
         aw: 1,
         dhm: 0,
       },
-      multipliers: [],
-    }),
+    ]),
     0
   );
 });
 
 test('PU of 11 Purified Water returns 1', (t) => {
   t.is(
-    calcPotionUnit({
-      base: {
+    calcPotionUnit([
+      {
         name: 'Purified Water',
         qtd: 11,
         pdh: 0,
         aw: 1,
         dhm: 0,
       },
-      multipliers: [],
-    }),
+    ]),
     1
   );
 });
 
 test('PU of 29 Purified Water returns 2', (t) => {
   t.is(
-    calcPotionUnit({
-      base: {
+    calcPotionUnit([
+      {
         name: 'Purified Water',
         qtd: 29,
         pdh: 0,
         aw: 1,
         dhm: 0,
       },
-      multipliers: [],
-    }),
+    ]),
     2
   );
 });
 
 test('PU of 33 Purified Water returns 3', (t) => {
   t.is(
-    calcPotionUnit({
-      base: {
+    calcPotionUnit([
+      {
         name: 'Purified Water',
         qtd: 33,
         pdh: 0,
         aw: 1,
         dhm: 0,
       },
-      multipliers: [],
-    }),
+    ]),
     3
   );
 });
 
 test('PU of 40 Purified Water returns 3', (t) => {
   t.is(
-    calcPotionUnit({
-      base: {
+    calcPotionUnit([
+      {
         name: 'Purified Water',
         qtd: 40,
         pdh: 0,
         aw: 1,
         dhm: 0,
       },
-      multipliers: [],
-    }),
+    ]),
     3
   );
 });
--- src/index.ts
+++ src/index.ts
@@ -19,27 +19,20 @@ export type Ingredient = {
   readonly am?: number;
 };
 
-export type Recipe = {
-  base: Ingredient;
-  multipliers: Ingredient[];
-};
-
 export type Potion = { pdh: number; pu: number };
 
-const calcPotionUnit = (recipe: Recipe): number =>
+const calcPotionUnit = (recipe: Ingredient[]): number =>
   Box(recipe)
-    .map(
-      ({ base, multipliers }) =>
-        base.aw * base.qtd +
-        multipliers.reduce((acc, cur) => cur.aw * cur.qtd + acc, 0)
+    .map((ingredients) =>
+      ingredients.reduce((acc, cur) => cur.aw * cur.qtd + acc, 0)
     )
     .fold((totalAW) => ((totalAW - 1) / 10) | 0);
 
-const calcTotalDissolution = ({ base, multipliers }: Recipe) =>
-  base.qtd + multipliers.reduce((acc, cur) => acc + cur.qtd, 0);
+const calcTotalDissolution = (recipe: Ingredient[]) =>
+  recipe.reduce((acc, cur) => acc + cur.qtd, 0);
 
-const calcTotalMultipliers = (multipliers: Ingredient[], dissolution: number) =>
-  multipliers.reduce(
+const calcTotalMultipliers = (recipe: Ingredient[], dissolution: number) =>
+  recipe.reduce(
     (acc, cur) =>
       !acc
         ? 1 + Math.sqrt(cur.qtd / dissolution) * cur.dhm
@@ -48,34 +41,30 @@ const calcTotalMultipliers = (multipliers: Ingredient[], dissolution: number) =>
   );
 
 const calcDirectHealing = (
-  { base, multipliers }: Recipe,
+  recipe: Ingredient[],
   dissolution: number
 ): number => {
   const herbalismFactor = (5 / 3) * 1.2; // 2
-  const multDH = multipliers.reduce(
+  const totalDH = recipe.reduce(
     (acc, cur) => acc + cur.pdh * (cur.qtd / dissolution),
     0
   );
 
-  return (
-    herbalismFactor *
-    ((base.pdh * base.qtd) / dissolution + multDH) *
-    (1 + Math.sqrt(base.qtd / dissolution) * base.dhm)
-  );
+  return herbalismFactor * totalDH;
 };
 
-const mix = ({ base, multipliers }: Recipe): Potion => {
-  const totalDissolution = calcTotalDissolution({ base, multipliers });
+const mix = (recipe: Ingredient[]): Potion => {
+  const totalDissolution = calcTotalDissolution(recipe);
 
-  const totalMultipliers = calcTotalMultipliers(multipliers, totalDissolution);
+  const totalMultipliers = calcTotalMultipliers(recipe, totalDissolution);
 
-  const calcDH = calcDirectHealing({ base, multipliers }, totalDissolution);
+  const calcDH = calcDirectHealing(recipe, totalDissolution);
 
   return {
     pdh: Number(
       (calcDH * (totalMultipliers ? totalMultipliers : 1)).toFixed(3)
     ),
-    pu: calcPotionUnit({ base, multipliers }),
+    pu: calcPotionUnit(recipe),
   };
 };
 
