2f9d41bf6dff2e444674f5bd1897e693277f8c75
Zetos Lima <cesar_manoel@hotmail.com>
2022-01-23 02:15:04 -0300

    fix: chain type.

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,6 @@
 type Functor<T> = {
   map: <U>(f: (y: T) => U) => Functor<U>;
-  chain: <U>(f: (y: T) => U) => U;
+  chain: <U>(f: (y: T) => Functor<U>) => Functor<U>;
   fold: <U>(f: (y: T) => U) => U;
   toString: string;
 };
@@ -10,7 +10,7 @@ type Box = <T>(x: T) => Functor<T>;
 const Box: Box = (arg) => ({
   map: (fn) => Box(fn(arg)),
   fold: (fn) => fn(arg),
-  chain: (fn) => fn(arg), //Box(Box(x))
+  chain: (fn) => fn(arg),
   toString: `Box(${arg})`,
 });
 
